c3d2221b2accb041f5b141ed5d993bbd0a024bc8
Aroshine <aroshine@kth.se>
2020-05-13 18:21:05 +0200

    testade
    ändrade färg för löpande texten i css
    ändra texten i js
    och la till två ord i index för introt på första start

--- main.js
+++ main.js
@@ -224,7 +224,7 @@ $( window ).resize( function() {
 $(function(){
 
         $("#typed").typed({
-            strings: ["web developer","geek","human being"], //Strings to display when typing
+            strings: ["engineer","and board member"], //Strings to display when typing
             typeSpeed: 40,
             backDelay: 600,
             loop: true,
