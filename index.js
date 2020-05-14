f0aa2687a009fb9ab83b524660b64ded0c4aba7c
Aroshine <aroshine@kth.se>
2020-05-14 22:46:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,8 @@ setTimeout(function(){
       $("#loading").css("display","none");
       $("#box").css("display","none");
       $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
       $("#work").removeClass("animated fadeIn");
+      $("#contact").removeClass("animated fadeIn");
+     
     },1000);
 },1500);
