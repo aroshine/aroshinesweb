da09b064834f759f7e6a32f293f177d4c2c00b1e
Aroshine <aroshine@kth.se>
2020-05-14 22:58:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ff3232"
+      "value": "#DAA520"
     },
     "shape": {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff3232"
+        "color": "#DAA520"
       },
       "polygon": {
         "nb_sides": 5
